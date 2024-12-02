661394e1356308631a45a205acc57d132ec8dd67
Muhammed Najin <najinn675@gmail.com>
2024-12-02 16:50:56 +0530

    Working version

--- clients/vite.config.ts
+++ clients/vite.config.ts
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    // Define global constants for env variables if needed
+    define: {
+      BASE_URL: JSON.stringify(env.URL),
+      PROFILE_URL: JSON.stringify(env.PROFILE_URL),
+      AUTH_URL: JSON.stringify(env.AUTH_URL)
     }
   }
 })
