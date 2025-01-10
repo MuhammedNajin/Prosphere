eeca6684e1a8c722b62885a2b086beda0b13ef4e
Muhammed Najin <najinn675@gmail.com>
2025-01-10 10:33:51 +0530

    chore: change the updating trail to company service

--- apiGateway-dev/src/proxy/proxy.ts
+++ apiGateway-dev/src/proxy/proxy.ts
@@ -23,35 +23,6 @@ export const setupProxies = (app: Application, routes: AuthRouteConfig[]) => {
                         proxyReq.setHeader('X-Admin-Data', JSON.stringify(req.currentAdmin));
                     } 
                 },
-                proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-                    const response = responseBuffer.toString('utf8');
-                     customLogger.info('logs', {  url: req.originalUrl, statu: res.statusCode ?? 'u'})
-                     const url = req.originalUrl;
-                     if(res.statusCode >= StatusCode.OK && res.statusCode < 300) {
-                        console.log("seccess res", url, "\n", response);
-
-                        if(url.includes(URL.JobPost)) {
-                            // customLogger.debug('job post ', { data: response });
-                            const data = JSON.parse(response)
-                            const companyId = data?.job.companyId;
-                            const { id, trail } = req.query; 
-                            customLogger.debug('job post dtails ', { companyId, stats: UsageMetrics.JobPostsUsed, query: req.query });
-                            if(trail === Trail_Status.TRUE) {
-                               console.log("heloo")
-                              try {
-                               await grpcPaymentClient.updateTrailLimit(id, UsageMetrics.JobPostsUsed)
-                              } catch (error) {
-                                console.log(" errror ", error);
-                                
-                              }
-                            } else if(trail === Trail_Status.False) {
-                               await grpcPaymentClient.updateFeaturesLimit(companyId, UsageMetrics.JobPostsUsed); 
-                            }
-                            
-                        } 
-                     }
-                    return response;
-                  }),
             }
         });
 
