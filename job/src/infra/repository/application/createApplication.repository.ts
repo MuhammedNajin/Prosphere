e93dad1394e054cf79935c67452a40f9a5737d41
Muhammed Najin <najinn675@gmail.com>
2025-01-16 17:46:36 +0530

    fix: Fixed the issue when one user already applied for job he can't apply for another

--- job/src/infra/repository/application/createApplication.repository.ts
+++ job/src/infra/repository/application/createApplication.repository.ts
@@ -1,10 +1,11 @@
 import { Application } from '@infra/database/mongo';
 import { IApplicationEntity } from '@domain/interface/IEntity';
+import { BadRequestError } from '@muhammednajinnprosphere/common';
 
 
 export class ApplicationCreationRepository {
      
     static async create(application: IApplicationEntity): Promise<IApplicationEntity | null> {
-        return await Application.build(application).save();
+        return (await Application.build(application).save()).populate(['jobId', 'companyId'])
     }
 }
--- job/src/presentation/handlers/controller/application/createApplication.controller.ts
+++ job/src/presentation/handlers/controller/application/createApplication.controller.ts
@@ -1,48 +1,61 @@
 import {
   ICreateApplicationUseCase,
-  IgetApplicationUseCase,
-  IgetMyApplicationUseCase,
+  IisAppliedUseCase,
 } from "@/application/interface/applicationUsecase_interface.ts";
+import { Dependency } from "@/infra/config/dependencies";
+import { winstonLogger } from "@/presentation/middleware/winstonLogger";
 import { StatusCode, ResponseUtil } from "@muhammednajinnprosphere/common";
 import { application, NextFunction, Request, Response } from "express";
 
 export class CreateApplicationController {
   constructor(
     private CreateApplicationUseCase: ICreateApplicationUseCase,
-    private GetMyApplicationUseCase: IgetMyApplicationUseCase
-  ) {}
+    private IsAppliedUseCase: IisAppliedUseCase,
+    private notificationProducer: Dependency["messageBroker"]["notificationProducer"]
+  ) {
+    console.log("notificationProducer", notificationProducer);
+  }
 
   public handler = async (req: Request, res: Response, next: NextFunction) => {
     try {
       console.log("req body", req.body);
-      const { jobId, companyId , applicantId } = req.body
+      const { jobId, companyId, applicantId } = req.body;
       console.log(jobId.length, companyId.length, applicantId.length);
-      
-      const payload = JSON.parse(req.headers["x-user-data"] as string);
-      console.log("payload", payload);
-      // const applicationExixts = await this.GetMyApplicationUseCase.execute(
-      //   payload.id
-      // );
 
-      // if (applicationExixts) {
-      //   res
-      //     .status(StatusCode.CONFLICT)
-      //     .json(
-      //       ResponseUtil.error(
-      //         StatusCode.CONFLICT,
-      //         "Only one application per candidate is allowed"
-      //       )
-      //     );
-      // }
+      const { id } = JSON.parse(req.headers["x-user-data"] as string);
+      console.log("id", id);
+
+      const applicationExixts = await this.IsAppliedUseCase.execute(id, jobId);
+
+      winstonLogger.info("applicationExixts", { applicationExixts });
+
+      if (applicationExixts) {
+        console.log("application already exists", applicationExixts)
+        return res
+          .status(StatusCode?.CONFLICT)
+          .json(
+            ResponseUtil.error("Only one application per candidate is allowed")
+          );
+      }
 
       const application = await this.CreateApplicationUseCase.execute(req.body);
+
+      await this.notificationProducer.produce({
+        actionUrl: `/my-application`,
+        data: {
+          applicationId: application?._id,
+        },
+        message: `Your application for ${application?.jobId?.jobTittle} at ${application?.companyId?.name} has been successfully submitted. We'll notify you when there's an update.`,
+        recipient: id,
+        title: "Application Received",
+        type: "application",
+      });
 
       res
         .status(StatusCode.CREATED)
         .json(
           ResponseUtil.success(application, "Application created successfully")
         );
-
     } catch (error) {
       console.log("controller", error);
       next(error);
