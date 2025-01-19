9a8cda05f1578180f4b6db93b2c1b83c584d821c
Muhammed Najin <najinn675@gmail.com>
2025-01-20 00:07:57 +0530

    feat: winston logger

--- payment/src/presentation/middlewate/loggerMiddleware.ts
+++ payment/src/presentation/middlewate/loggerMiddleware.ts
@@ -1,24 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
+
 import { createCustomLogger } from '@muhammednajinnprosphere/common';
-import winston from 'winston';
 
 
 
-declare global {
-    namespace Express {
-        export interface Request {
-            logger?: winston.Logger,
-        }
-    }
-}
+export const customLogger = createCustomLogger('payment-service');
 
 
-const customLogger = createCustomLogger('payment-service', __dirname);
 
-const loggerMiddleware = (req: any, res: Response, next: NextFunction) => {
-    req.logger = customLogger;
-    next();
-};
-
-
-export default loggerMiddleware;
