4fd21ca560c32085590a74feb11eac8a9c81d9da
Muhammed Najin <najinn675@gmail.com>
2025-02-12 15:29:43 +0530

    chore: unused import removed

--- client/src/components/job/JobApplicationForm.tsx
+++ client/src/components/job/JobApplicationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Paperclip, CircleCheck, AlertCircle } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
